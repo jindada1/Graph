c414d6e389e502b81d81fcaa2fd8872a1cf41b4f
jindada1 <zou_406143883@qq.com>
2021-07-15 14:42:30 +0800

    bayes

--- components/bayes.js
+++ components/bayes.js
@@ -20,7 +20,8 @@ Vue.component('gh-bayes', {
                 <el-divider content-position="center">图像属性</el-divider>
                 <kris-color-picker v-model="plotConfig.diagnosedColor" title="诊断为阳性的颜色"></kris-color-picker>
                 <kris-color-picker v-model="plotConfig.unDiagnosedColor" title="诊断为阴性的颜色"></kris-color-picker>
-                <kris-color-picker v-model="plotConfig.barColor" title="柱的颜色"></kris-color-picker>
+                <kris-color-picker v-model="plotConfig.emulateBarColor" title="模拟结果的颜色"></kris-color-picker>
+                <kris-color-picker v-model="plotConfig.theoryBarColor" title="理论结果的颜色"></kris-color-picker>
                 <kris-slider v-model="plotConfig.barWidth" title="柱的宽度" :min="10" :max="100" :step="1"></kris-slider>
                 <kris-slider v-model="plotConfig.maxBarHeight" title="柱的最大高度" :min="100" :max="600" :step="10"></kris-slider>
             </template>
@@ -55,15 +56,22 @@ Vue.component('gh-bayes', {
                     </el-col>
                 </el-row>
                 <el-row class="kris-icons-row">
-                    <el-col :span="6" v-for="(n, i) in result" :key="i"> {{n}} </el-col>
+                    <el-col :span="6" v-for="(res, i) in result" :key="i"> {{res[0]}} / {{res[1]}} </el-col>
                 </el-row>
                 <el-row class="kris-icons-row" :style="'height:'+ plotConfig.maxBarHeight +'px'">
                     <el-col :span="6" v-for="(h, index) in barHeights" :key="index">
-                        <div class="kris-col-bar" :style="
-                            'height:'+ h +'px;' +  
-                            'background-color:'+ plotConfig.barColor + ';' + 
-                            'width:' + plotConfig.barWidth + 'px'
-                        "></div>
+                        <div class="kris-multibar-col">
+                            <div class="kris-col-bar" :style="
+                                'height:'+ h[0] +'px;' +  
+                                'background-color:'+ plotConfig.emulateBarColor + ';' + 
+                                'width:' + plotConfig.barWidth + 'px'
+                            "></div>
+                            <div class="kris-col-bar" :style="
+                                'height:'+ h[1] +'px;' +  
+                                'background-color:'+ plotConfig.theoryBarColor + ';' + 
+                                'width:' + plotConfig.barWidth + 'px'
+                            "></div>
+                        </div>
                     </el-col>
                 </el-row>
             </template>
@@ -82,10 +90,11 @@ Vue.component('gh-bayes', {
             },
             plotConfig: {
                 diagnosedColor: "#409EFF",
-                unDiagnosedColor: "#DDDDDD",
+                unDiagnosedColor: "#005F08",
                 barWidth: 40,
-                barColor: "#79BBFF",
                 maxBarHeight: 200,
+                emulateBarColor: "#925500",
+                theoryBarColor: "#4B0070"
             },
             order: [
                 [true, true],
@@ -94,7 +103,8 @@ Vue.component('gh-bayes', {
                 [false, false],
             ],
             precise: 2,
-            result: [],
+            emulateResult: [],
+            theoryResult: [],
             storageList: [
                 "experiment",
                 "plotConfig"
@@ -122,9 +132,17 @@ Vue.component('gh-bayes', {
     },
     computed: {
         barHeights() {
-            let result = Object.values(this.result)
-            let MH =  Math.max(...result);
-            return result.map(num => parseInt(this.plotConfig.maxBarHeight * num / MH));
+            let TMV = Math.max(...this.theoryResult);
+            // 所有结果的最大值
+            let MV = TMV;
+            // 柱状图的最大高度
+            let MH = this.plotConfig.maxBarHeight;
+            let ERes = this.emulateResult.map(num => parseInt(MH * num / MV));
+            let TRes = this.theoryResult.map(num => parseInt(MH * num / MV));
+            return this.enumerate(ERes, TRes)
+        },
+        result() {
+            return this.enumerate(this.emulateResult, this.theoryResult);
         },
         NN() { return this.experiment.NN.toFixed(this.precise) },
         NP() { return (1 - this.experiment.NN).toFixed(this.precise) },  // 实际是阴性但诊断成阳性的概率
@@ -132,6 +150,15 @@ Vue.component('gh-bayes', {
         PN() { return (1 - this.experiment.PP).toFixed(this.precise) },  // 实际是阳性但诊断成阴性的概率
     },
     methods: {
+        enumerate(arr1, arr2) {
+            let res = []
+            if (arr1.length === arr2.length) {
+                for (let i = 0; i < arr1.length; i++) {
+                    res.push([arr1[i], arr2[i]])
+                }
+            }
+            return res;
+        },
         display() {
             if (this.updateLock) return;
             this.emulate();
@@ -147,8 +174,15 @@ Vue.component('gh-bayes', {
             let NNums = this.divide(K, this.NN)
             // 试图诊断阴性
             let PNums = this.divide(N - K, this.PP)
-            
-            this.result = [NNums[1], NNums[0], ...PNums];
+            this.emulateResult = [NNums[1], NNums[0], ...PNums];
+
+            // 计算理论值
+            this.theoryResult = [
+                K * this.NP,
+                K * this.experiment.NN,
+                (N - K) * this.experiment.PP,
+                (N - K) * this.PN
+            ];
         },
         divide(num, p) {
             let hit = 0
