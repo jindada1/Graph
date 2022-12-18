8ad5f24c2afbc59464f344f737ba6ad1ea376eb5
Kris Huang <zou_406143883@qq.com>
2022-12-18 13:51:35 +0800

    补充

--- 二期/src/components/zerooneSL.js
+++ 二期/src/components/zerooneSL.js
@@ -1,32 +1,29 @@
- Vue.component('gh-normalSL', {
+ Vue.component('gh-zerooneSL', {
     template: `
         <kris-layout ref="frame">
             <template v-slot:left>
-                <el-divider content-position="center">${Lang.normalSL.section_distribution_name}</el-divider>
-                <kris-num-input title="${Lang.normalSL.null_hypotheses}" v-model="experiment.avg" :step="1"></kris-num-input>
-                <kris-form-item title="${Lang.normalSL.alternative_hypotheses}" :value="'μ ≠ ' + experiment.avg.toString()" :step="1"></kris-form-item>
-                <el-divider content-position="center">${Lang.normalSL.section_experiment_name}</el-divider>
-                <kris-num-input title="${Lang.normalSL.sample_num}" v-model="experiment.sampleNum" :step="10"></kris-num-input>
-                <kris-num-input v-model="graphConfig.level" :min="0.01" :max="0.5" :step="0.01" title="${Lang.normalSL.significant_level}"></kris-num-input>
-                <kris-num-input title="${Lang.normalSL.try_times}" v-model="experiment.epoch" :step="10"></kris-num-input>
-                <kris-button title="${Lang.normalSL.view_table}" type="text" :click="showTable"></kris-button>
-                <el-dialog class="kris-table-dialog" title="${Lang.normalSL.table_name}" :visible.sync="dialogTableVisible">
+                <el-divider content-position="center">${Lang.zerooneSL.section_distribution_name}</el-divider>
+                <kris-num-input title="${Lang.zerooneSL.null_hypotheses}" v-model="experiment.avg" :step="0.01" :min="0" :max="1"></kris-num-input>
+                <kris-form-item title="${Lang.zerooneSL.alternative_hypotheses}" :value="'p ≠ ' + experiment.avg.toString()" :step="1"></kris-form-item>
+                <el-divider content-position="center">${Lang.zerooneSL.section_experiment_name}</el-divider>
+                <kris-num-input title="${Lang.zerooneSL.sample_num}" v-model="experiment.sampleNum" :step="10"></kris-num-input>
+                <kris-num-input v-model="graphConfig.level" :min="0.01" :max="0.5" :step="0.01" title="${Lang.zerooneSL.significant_level}"></kris-num-input>
+                <kris-num-input title="${Lang.zerooneSL.try_times}" v-model="experiment.epoch" :step="10"></kris-num-input>
+                <kris-button title="${Lang.zerooneSL.view_table}" type="text" :click="showTable"></kris-button>
+                <el-dialog class="kris-table-dialog" title="${Lang.zerooneSL.table_name}" :visible.sync="dialogTableVisible">
                     <kris-table :title="table.title" :header="table.header" :rows="table.rows"></kris-table>
                 </el-dialog>
 
-                <el-divider content-position="center">${Lang.normalSL.display_props}</el-divider>
-                <kris-slider v-model="layoutConfig.formulaHeight" :min="50" :max="90" title="${Lang.normalSL.formula_size}"></kris-slider>
+                <el-divider content-position="center">${Lang.zerooneSL.display_props}</el-divider>
+                <kris-slider v-model="layoutConfig.formulaHeight" :min="50" :max="90" title="${Lang.zerooneSL.formula_size}"></kris-slider>
             </template>
             <template v-slot:right>
                 <div style="height: 100%; margin: 0 30px;">
                     <div style="height: 220px;">
-                        <div> ${ Lang.normalSL.formula_description } </div>
+                        <div> ${ Lang.zerooneSL.formula_description } </div>
                         <div style="display: flex; align-items: center; margin: 30px 0;"> 
-                            ${ Lang.normalSL.deny_area }
-                            <img src='./static/images/deny_area.svg' :height="layoutConfig.formulaHeight + 'px'" />
-                            <div style="flex: 1"></div>
-                            <img src='./static/images/s.svg' :height="layoutConfig.formulaHeight + 'px'" />
-                            <div style="flex: 1"></div>
+                            ${ Lang.zerooneSL.deny_area }
+                            <img src='./static/images/aeroone_deny_area_mu.svg' :height="layoutConfig.formulaHeight + 'px'" />
                         </div>
                         <div style="justify-content: space-between" class="equations">
                             <div>
@@ -38,58 +35,54 @@
                                 <div> {{ alpha }} </div>
                             </div>
                             <div>
-                                <img src='./static/images/t_1-1_2alpha.svg'/>
+                                <img src='./static/images/mu_1-1_2alpha.svg'/>
                                 <div style="margin-bottom: 6px;"> {{ t }} </div>
                             </div>
                             <div> </div>
                         </div>
                     </div>
                     <div style="position: relative; height: calc(100% - 220px); min-height: 400px">
-                        <el-descriptions class="margin-top" title="${Lang.normalSL.result1_title}" :column="2" border>
+                        <el-descriptions class="margin-top" title="${Lang.zerooneSL.result1_title}" :column="2" border>
                             <template slot="extra">
                                 <el-button type="primary" size="small" @click="calculateData">
-                                    ${Lang.normalSL.result_btn_text}
+                                    ${Lang.zerooneSL.result_btn_text}
                                 </el-button>
                             </template>
-                            <el-descriptions-item label="${Lang.normalSL.result1_accept}">
+                            <el-descriptions-item label="${Lang.zerooneSL.result1_accept}">
                                 {{result.a}}
                             </el-descriptions-item>
-                            <el-descriptions-item label="${Lang.normalSL.result1_deny}">
+                            <el-descriptions-item label="${Lang.zerooneSL.result1_deny}">
                                 {{experiment.epoch - result.a}}
                             </el-descriptions-item>
-                            <el-descriptions-item label="${Lang.normalSL.result1_frequency}">
+                            <el-descriptions-item label="${Lang.zerooneSL.result1_frequency}">
                                 {{((experiment.epoch - result.a) / 100).toFixed(2)}}
                             </el-descriptions-item>
                             <el-descriptions-item label="注">
-                                ${Lang.normalSL.result1_tips_1}
+                                ${Lang.zerooneSL.result1_tips_1}
                                 {{experiment.avg}}
-                                ${Lang.normalSL.result1_tips_2} 
-                                {{Math.abs(experiment.avg) / 10}}
-                                ${Lang.normalSL.result1_tips_3} 
+                                ${Lang.zerooneSL.result1_tips_3} 
                             </el-descriptions-item>
                         </el-descriptions>
                         <div style="height: 50px"></div>
-                        <el-descriptions class="margin-top" title="${Lang.normalSL.result2_title}" :column="2" border>
+                        <el-descriptions class="margin-top" title="${Lang.zerooneSL.result2_title}" :column="2" border>
                             <template slot="extra">
                                 <el-button type="primary" size="small" @click="calculateData">
-                                    ${Lang.normalSL.result_btn_text}
+                                    ${Lang.zerooneSL.result_btn_text}
                                 </el-button>
                             </template>
-                            <el-descriptions-item label="${Lang.normalSL.result2_accept}">
+                            <el-descriptions-item label="${Lang.zerooneSL.result2_accept}">
                                 {{result.b}}
                             </el-descriptions-item>
-                            <el-descriptions-item label="${Lang.normalSL.result2_deny}">
+                            <el-descriptions-item label="${Lang.zerooneSL.result2_deny}">
                                 {{experiment.epoch - result.b}}
                             </el-descriptions-item>
-                            <el-descriptions-item label="${Lang.normalSL.result2_frequency}">
+                            <el-descriptions-item label="${Lang.zerooneSL.result2_frequency}">
                                 {{((experiment.epoch - result.b) / 100).toFixed(2)}}
                             </el-descriptions-item>
                             <el-descriptions-item label="注">
-                                ${Lang.normalSL.result2_tips_1}
-                                {{experiment.avg * 1.1}}
-                                ${Lang.normalSL.result2_tips_2} 
-                                {{Math.abs(experiment.avg) / 10}}
-                                ${Lang.normalSL.result2_tips_3} 
+                                ${Lang.zerooneSL.result2_tips_1}
+                                {{experiment.avg * 0.8}}
+                                ${Lang.zerooneSL.result2_tips_3} 
                             </el-descriptions-item>
                         </el-descriptions>
                     </div>
@@ -99,7 +92,7 @@
     `,
     data() {
         return {
-            componentName: "gh-normalSL",
+            componentName: "gh-zerooneSL",
             updateLock: false,
             inited: false,
             table: {
@@ -115,7 +108,7 @@
             alpha: "",
             t: "",
             experiment: {
-                avg: 4,
+                avg: 0.5,
                 sampleNum: 50,  // 样本容量
                 precise: 0.1,   // 采样精度
                 epoch: 50       // 采样次数
@@ -159,14 +152,11 @@
             return Math.sqrt(_avg / 10);
         },
         // 生成样本的采样函数
-        sample(avg) {
-            let bm = BoxMuller();
-            const { sampleNum } = this.experiment
+        sample(p) {
             let samples = [];
-            
+            const { sampleNum } = this.experiment;
             for (let index = 0; index < sampleNum; index++) {
-                let n = bm(avg, this.calcStd(avg));
-                samples.push(n)
+                samples.push(Math.random() > p ? 0 : 1)
             }
             return samples
         },
@@ -186,7 +176,7 @@
             this.calcA();
             this.calcB();
         },
-        calcA(){
+        calcA() {
             const {epoch, avg} = this.experiment;
             let a = 0;
             // 采样 epoch 次
@@ -194,17 +184,12 @@
                 const samples = this.sample(avg);
                 // 计算此次采样的均值
                 const mean = meanOfArray(samples)
-                // 此次采样的方差
-                const variance = samples.reduce((s, x) => s + (mean - x) * (mean - x), 0) / (samples.length - 1);
-                // 标准差
-                const s = Math.sqrt(variance);
-                // 标准化
-                const std_mean = math.abs(mean - avg) / s;
                 // 左边
-                const left = Math.sqrt(samples.length) * std_mean;
+                const temp = Math.abs(mean - avg) / Math.sqrt(avg * (1- avg))
+                const left = Math.sqrt(samples.length) * temp;
                 // 右边
                 const right = this.t
-                if (left < right) {
+                if (left > right) {
                     a += 1
                 }
             }
@@ -212,26 +197,20 @@
         },
         calcB(){
             const { epoch } = this.experiment;
-            const avg = this.experiment.avg * 1.1;
+            const avg = this.experiment.avg * 0.8;
             // console.log(avg);
-
             let b = 0;
             // 采样 epoch 次
             for (let index = 0; index < epoch; index++) {
                 const samples = this.sample(avg);
                 // 计算此次采样的均值
                 const mean = meanOfArray(samples)
-                // 此次采样的方差
-                const variance = samples.reduce((s, x) => s + (mean - x) * (mean - x), 0) / (samples.length - 1);
-                // 标准差
-                const s = Math.sqrt(variance);
-                // 标准化
-                const std_mean = math.abs(mean - avg) / s;
                 // 左边
-                const left = Math.sqrt(samples.length) * std_mean;
+                const temp = Math.abs(mean - avg) / Math.sqrt(avg * (1- avg))
+                const left = Math.sqrt(samples.length) * temp;
                 // 右边
                 const right = this.t
-                if (left < right) {
+                if (left > right) {
                     b += 1
                 }
             }
